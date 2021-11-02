9992ed532675ec8b22c69797ff15f9041d784a21
ahmedalaa000 <93440763+ahmedalaa000@users.noreply.github.com>
2021-11-03 01:20:31 +0200

    Add files via upload

---
+++ Ahmed alaa .m
@@ -0,0 +1,36 @@
+A=[-7 5 -9;2 -1 2;1 -1 2];
+B=[16 3 2 13;5 10 11 8 ;9 6 7 12;4 15 14 1];
+C=[4 2 -3;7 -7 9 ;3 -5 6];
+D=[6 3 2;2 12 -7;-1 6 2;-5 15 11];
+q=3*A-5*C;
+t=7*A+2*B; %Error using  + 
+            Matrix dimensions must agree.
+z=C*A;
+f=C*D';                      
+%Q2
+%n=7,8
+b=zeros(7);
+p=zeros(7,8);
+c=ones(7);
+D=ones(7,8);
+[r,x]=size(d);
+j=zeros(7,8);
+n=diag([1 2 3 4]);
+h=eye(7);
+%Q3                     
+[A,B]; %Error using horzcat
+Dimensions of matrices being concatenated are not consistent.
+[A;B]; %Error using vertcat
+Dimensions of matrices being concatenated are not consistent.
+%Q4                       
+Y = diag([5 5 5 5 5 5 5 5]);Y(:,8)=[5;5;5;5;5;5;5;5];
+%Q5                  
+ A(2,:)=[1 2 3];
+A(:,2)=[4 5 6];                     
+                      
+                      
+                      
+                      
+                      
+                      
+                      
